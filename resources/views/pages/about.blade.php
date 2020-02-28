515d4dae73bac3b7170a90e1272ea4b5d3ae02b3
Mike Shilovski <ShMike6491@gmail.com>
2020-02-28 16:20:07 +0300

    initial import

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <?php echo"<h1>$title</h1>"; ?>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis finibus augue, a viverra nisl 
+        molestie a. Aenean eu dui finibus, efficitur ipsum non, dignissim tellus. Proin pellentesque, 
+        arcu eu blandit lobortis, elit nulla pulvinar justo, ac imperdiet libero erat vitae diam. 
+        Praesent accumsan id nunc ac cursus. Sed cursus ipsum vitae nisl scelerisque, et imperdiet 
+        dolor placerat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+        Curae; Praesent metus nulla, facilisis eu dolor eu, facilisis blandit sapien. Ut nisi urna, 
+        tincidunt vitae mi vitae, scelerisque congue nisl. Pellentesque velit est, bibendum id maximus 
+        eget, euismod at massa. Quisque aliquam laoreet mattis. Phasellus malesuada massa sit amet arcu 
+        dignissim, ac tempus neque condimentum. Aenean porta lobortis lorem, sit amet vehicula lectus 
+        aliquet sed.
+        <br>
+        <br>
+        Nunc tincidunt interdum tortor quis lacinia. Pellentesque sodales felis lacus. 
+        Phasellus vehicula semper vulputate. Orci varius natoque penatibus et magnis dis parturient 
+        montes, nascetur ridiculus mus. Duis semper mauris quis fermentum faucibus. Vivamus consequat 
+        ex ac diam faucibus ullamcorper. Suspendisse potenti. Nam consequat malesuada sapien congue 
+        varius. Integer lacinia sem vitae eleifend varius.
+        <br>
+        <br>
+        Suspendisse lobortis felis in ex feugiat auctor. Maecenas nec interdum enim. 
+        Maecenas iaculis cursus blandit. Ut nec fringilla orci. Vivamus a bibendum elit. 
+        Nunc sed erat eu nunc varius porttitor. Nunc euismod convallis mattis. Vestibulum vulputate 
+        rhoncus leo tincidunt elementum. Suspendisse sit amet tellus et tortor condimentum 
+        semper nec ultrices metus. Nam interdum purus erat, vitae commodo odio rhoncus eget.
+        <br>
+        <br>
+        Integer at dictum metus, id euismod odio. Vestibulum vehicula lacus sed dui consectetur, 
+        et euismod neque aliquet. Vivamus rhoncus eros vulputate pretium ornare. Vestibulum ante diam, 
+        sagittis pulvinar lectus non, porttitor mollis lorem. Nullam tincidunt convallis eros, vitae 
+        congue augue tempus in. Nullam at tellus metus. Morbi ut dignissim velit. Phasellus semper metus
+         ut mi aliquam venenatis. Aenean gravida efficitur dui a laoreet. Phasellus sagittis id odio ut 
+         auctor. Duis volutpat est non lacus tincidunt vestibulum in eu purus.
+        <br>
+        <br>
+        Integer luctus porta mi, vitae eleifend enim cursus vel. Duis elementum ante nec ex tristique, 
+        vel laoreet erat rhoncus. Nullam elementum dui enim, pharetra ornare lacus gravida sit amet. 
+        Vivamus in elit imperdiet arcu condimentum pretium at quis lorem. Nullam semper ipsum libero, 
+        at pharetra nibh sodales et. Duis auctor magna est, id hendrerit augue pretium at. Nam id 
+        erat quis leo fermentum hendrerit. Nulla porttitor tortor urna, ut tincidunt nunc ultricies a. 
+        Ut porta urna a enim suscipit ultricies. Sed tellus arcu, sodales nec fringilla et, congue vel 
+        mauris. Duis vitae pretium turpis, a scelerisque ex. Maecenas neque purus, dapibus maximus 
+        sodales quis, tincidunt eget enim. Sed vel mollis ligula, vitae fringilla nibh. Quisque euismod 
+        consequat nisi, in tincidunt lorem laoreet eget. Sed feugiat leo dui, vitae hendrerit nunc 
+        lobortis eu. Nunc ornare scelerisque molestie.</p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome to The Laravel Site</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <p>
+            <a href="/login" class="btn btn-secondary btn-lg">Login</a>
+            <a href="/register" class="btn btn-secondary btn-lg">Signup</a>
+        </p>
+    </div>
+@endsection
